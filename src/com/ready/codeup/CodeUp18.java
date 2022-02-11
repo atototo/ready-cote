f861a65a36509238a743c20d0caf54e2de137bb1
yelee <yelee@crizen.com>
2022-02-11 10:46:53 +0900

    codeup 1021 문제

--- src/com/ready/codeup/CodeUp18.java
+++ src/com/ready/codeup/CodeUp18.java
@@ -1,8 +1,15 @@
-package com.ready;
+package com.ready.codeup;
 
 import java.util.Scanner;
 
-public class Main {
+/**
+ * 주민번호 앞 6자리와 뒷 7자리가 '-'로 구분되어 입력된다.
+ * (입력값은 가상의 주민번호이다.)
+ * ex)110011-0000000
+ *  ex : 000907-1121112
+ *  result : 0009071121112
+ */
+public class CodeUp18 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
